82f4ffab129e77c97ebd427da0b33f4b7f18d2ca
puethe <cp15@sanger.ac.uk>
2018-07-26 15:08:58 +0100

    set root permission

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -5,8 +5,8 @@ sudo apt-get update
 sudo apt-get install postgresql postgresql-client postgresql-contrib
 
 # Setup PostgreSQL and create a new user
-update-rc.d postgresql enable
-su - postgres
+sudo update-rc.d postgresql enable
 service postgresql start 
+su - postgres
 psql --command "CREATE USER chado_user WITH SUPERUSER PASSWORD 'chado_pw';" \
 createdb -O chado_user chado_user
